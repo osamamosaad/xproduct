a4f1f62a314f8e3a9c5794dea2f5d0a60813e10e
Osama Mosaad <osama.mosaad@basharsoft.com>
2021-12-26 18:21:37 +0200

    Added infrastructure Exceptions

---
+++ src/xproduct/Infrastructure/Exceptions/ForbiddenException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Xproduct\Infrastructure\Exceptions;
+
+use App\Exceptions\ForbiddenException as Forbidden;
+
+class ForbiddenException extends Forbidden
+{
+}
---
+++ src/xproduct/Infrastructure/Exceptions/NotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Xproduct\Infrastructure\Exceptions;
+
+use App\Exceptions\NotFoundException as NotFound;
+
+class NotFoundException extends NotFound
+{
+}
