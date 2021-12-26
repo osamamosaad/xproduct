186eb68fe624ace79f9b98c15e3a632ba14911cc
Osama Mosaad <osama.mosaad@basharsoft.com>
2021-12-26 17:32:50 +0200

    Created discount table

---
+++ src/database/migrations/2021_12_25_210104_discount.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class Discount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement(
+            "CREATE TABLE `discount` (
+                `id` int NOT NULL AUTO_INCREMENT,
+                `type` enum('category','sku') COLLATE utf8mb4_general_ci NOT NULL,
+                `item` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+                `percentage` tinyint NOT NULL,
+                `created_at` datetime NOT NULL,
+                `updated_at` datetime NOT NULL,
+                KEY `discount_ix_type_item` (`type`,`item`),
+            PRIMARY KEY (`id`)) ENGINE=InnoDB;"
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
