1671c4c42e614e81f43717f824d1629d69151358
Osama Mosaad <osama.mosaad@basharsoft.com>
2021-12-26 23:01:47 +0200

    Added const for prameter types

--- src/xproduct/Libraries/Clients/ProductClient.php
+++ src/xproduct/Libraries/Clients/ProductClient.php
@@ -8,6 +8,9 @@ use Xproduct\Libraries\Product\Repositories\ProductRepositoryInterface;
 
 class ProductClient implements ProductClientInterface
 {
+    public const PARAMETER_TYPE_CATEGORY = "category";
+    public const PARAMETER_TYPE_SKU = "sku";
+
     public function __construct(
         private ProductRepositoryInterface $productRepo,
         private CategoryRepositoryInterface $categorytRepo,
@@ -20,10 +23,10 @@ class ProductClient implements ProductClientInterface
     public function getProductsByParameterType(string $type, string $value): array
     {
         switch ($type) {
-            case "category":
+            case self::PARAMETER_TYPE_CATEGORY:
                 $categoryId = $this->categorytRepo->getCategoryIdByName($value);
                 return $this->productRepo->getByCategory($categoryId);
-            case "sku":
+            case self::PARAMETER_TYPE_SKU:
                 return $this->productRepo->getBySku($value);
             default:
                 throw new NotFoundException("this type {$type} is not found.");
