b022a3e2d9aff727feffe7cd708eb6485068e94f
Osama Mosaad <osama.mosaad@basharsoft.com>
2021-12-26 18:51:20 +0200

    Added addProduct command in the application layer for future use

---
+++ src/xproduct/Application/Commands/AddProduct.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Xproduct\Application\Commands;
+
+class AddProduct
+{
+    /**
+     * To handle product data that comes from presentation layer
+     *
+     * @param array $data
+     *
+     * @return void
+     */
+    public function execute(array $data): void
+    {
+        // for future
+    }
+}
